05b233f5822c195f34927d5a123486d7b47f08a6
Thijs Boom <twboom@hotmail.nl>
2021-03-12 22:53:13 +0100

    Made it sorta working

--- src/js/play.js
+++ src/js/play.js
@@ -14,5 +14,39 @@ synthquencer.sound = function(note, wave) {
     oscillator.connect(gainNode);
     gainNode.connect(interface.destination);
     oscillator.start(0);
-    oscillator.stop(interface.currentTime + 2)
+    oscillator.stop(interface.currentTime + 0.5)
+}
+
+synthquencer.playRow = function(step) {
+    const row = document.querySelectorAll(`button.tile[data-step="${step}"][data-activated=true]`)
+    if (row.length === 0) { return }
+    const wave = row[0].getAttribute('data-wave')
+    for (let i = 0; i < row.length; i++) {
+        const note = (row[i].getAttribute('data-note'))
+        synthquencer.sound(note, wave)
+    }
+}
+
+synthquencer.run = function() {
+    let clock;
+    let thisRow = 0
+    let lastRow;
+    clock = setInterval(_ => {
+        synthquencer.playRow(thisRow)
+        {
+            const row = document.querySelectorAll(`button.tile[data-step="${thisRow}"]`)
+            for (let i = 0; i < row.length; i++) {
+                row[i].setAttribute('data-isplaying', true)
+            }
+        }
+        {
+            const row = document.querySelectorAll(`button.tile[data-step="${lastRow}"]`)
+            for (let i = 0; i < row.length; i++) {
+                row[i].setAttribute('data-isplaying', false)
+            }
+        }
+        lastRow = thisRow
+        thisRow += 1
+        if (thisRow == 16) { thisRow = 0}
+    }, 100)
 }
--- src/js/synth.js
+++ src/js/synth.js
@@ -10,21 +10,21 @@ synthquencer.tile = class {
         const id = `${this.wave}-${this.note}-${this.step}`
         const tile = document.createElement('button')
         tile.setAttribute('class', 'tile')
-        tile.setAttribute('wave', this .wave)
-        tile.setAttribute('note', this.note)
-        tile.setAttribute('step', this.step)
-        tile.setAttribute('activated', false)
-        tile.setAttribute('isPlaying', false)
+        tile.setAttribute('data-wave', this .wave)
+        tile.setAttribute('data-note', this.note)
+        tile.setAttribute('data-step', this.step)
+        tile.setAttribute('data-activated', false)
+        tile.setAttribute('data-isPlaying', false)
         tile.setAttribute('id', id)
         tile.addEventListener('click', _ => {
             if (this.activated) {
                 this.activated = false;
-                tile.setAttribute('activated', 'false')
+                tile.setAttribute('data-activated', 'false')
                 console.log(`Deactivated ${this.wave}, ${this.note}, ${this.step}`)
             }
             else {
                 this.activated = true;
-                tile.setAttribute('activated', 'true')
+                tile.setAttribute('data-activated', 'true')
                 console.log(`Activated ${this.wave}, ${this.note}, ${this.step}`)
             }
         })
@@ -44,6 +44,6 @@ synthquencer.frame.sine = function() {
             const tile = new synthquencer.tile('sine', note, i)
             tile.create(step)
         }
-        document.getElementById('frame').appendChild(step)
+        document.getElementById('sequencer').appendChild(step)
     }
 }
