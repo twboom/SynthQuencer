df85a836f19c7abfbfa6c9dd6280c7f16110d7fb
Thijs Boom <twboom@hotmail.nl>
2021-03-13 15:01:40 +0100

    Working on good inputs

--- src/js/interface.js
+++ src/js/interface.js
@@ -1,3 +1,17 @@
+synthquencer.interface = [];
+
+synthquencer.interface.update = function(evt) {
+    const input = evt.target;
+    const display = document.querySelector(`span.${input.id}.display`)
+    
+}
+
+document.querySelectorAll('input.control').forEach(item => {
+    item.addEventListener('mouseup', evt => {
+
+    })
+})
+
 // Toggle button
 document.querySelector('button#toggle.controls').addEventListener('click', _ => {
     for (let i = 0; i < synthquencer.synths.length; i++) {
