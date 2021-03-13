efa3708bfcb98b68156ce9a6f86ed9b53d29e5f8
Thijs Boom <twboom@hotmail.nl>
2021-03-13 13:18:58 +0100

    Small cleanup

--- src/js/interface.js
+++ src/js/interface.js
@@ -1,5 +1,5 @@
 // Toggle button
-document.querySelector('button#toggle.controls').addEventListener('click', evt => {
+document.querySelector('button#toggle.controls').addEventListener('click', _ => {
     for (let i = 0; i < synthquencer.synths.length; i++) {
         synthquencer.run(i)
     }
