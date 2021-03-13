67e89ac7b948cf73a5e9761a0a983ad85213f452
Thijs Boom <twboom@hotmail.nl>
2021-03-13 11:59:29 +0100

    Added state object

--- src/js/synthquencer.js
+++ src/js/synthquencer.js
@@ -1,11 +1,15 @@
 let synthquencer = [];
 
-synthquencer.config = {
+synthquencer.config = { // Configurable settings
     'startNote': 60, // The lowest note, in MIDI note
     'notes': 16, // Number of notes in step
     'steps': 16, // The number of steps
     'speed': 500, // Interval between steps, in ms
     'tuning': 440 // Tuning of A4, in hz
+}
+
+synthquencer.state = { // States of SynthQuencer
+    'active': false
 }
 
 synthquencer.synth = { // Not yet finished / active
