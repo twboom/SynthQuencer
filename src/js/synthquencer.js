3e950a7594791babd38bd8056cfc65c481ccea48
Thijs Boom <twboom@hotmail.nl>
2021-03-24 12:15:26 +0100

    Added default wave to config Fixes #24

--- src/js/synthquencer.js
+++ src/js/synthquencer.js
@@ -6,7 +6,8 @@ synthquencer.config = { // Configurable settings
     'steps': 16, // The number of steps
     'speed': 100, // Interval between steps, in ms
     'tuning': 440, // Tuning of A4, in Hz
-    'transposition': 0 // Transpose all the grids, in octaves
+    'transposition': 0, // Transpose all the grids, in octaves
+    'defaultWave': 'sawtooth' // The default wave value when no wave is specified
 };
 
 synthquencer.state = { // States of SynthQuencer
