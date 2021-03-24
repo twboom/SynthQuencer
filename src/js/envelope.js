d17072073a0522164c06f76e81a86e01e64e7897
Thijs Boom <twboom@hotmail.nl>
2021-03-24 10:33:38 +0100

    Finialized it

--- src/js/envelope.js
+++ src/js/envelope.js
@@ -4,33 +4,5 @@ synthquencer.envelope.envelope = {
     'attack': 0,
     'decay': 1,
     'sustain': 0.01,
-    'release': 1
+    'release': 0
 };
-
-synthquencer.envelope.config = {
-    'attack': {
-        'min': 0,
-        'max': 10
-    },
-    'decay': {
-        'min': 0,
-        'max': 10
-    },
-    'sustain': {
-        'min': 0,
-        'max': 10
-    },
-    'release': {
-        'min': 0,
-        'max': 10
-    }
-}
-
-synthquencer.envelope.update = function(property, value) {
-    const envelope = synthquencer.envelope.envelope[property];
-    const config = synthquencer.envelope.config[property];
-
-    if (config.min <= value <= config.max) { envelope = value}
-
-    throw new Error('Value is invalid')
-}
